5f3ea1231145aed965cfaf04f38273f429d862e5
Kamaal <mkamaalg4@gmail.com>
2019-10-23 23:38:08 +0530

    submit

---
+++ 034_Mustafa_LAB6_Q8.py
@@ -0,0 +1,12 @@
+# Name: Mohd Mustafa Kamaal
+# Roll no: 34
+# Enrl no: A180571
+# Batch: 1
+print("For Performing Palindrome Operations")
+n=input("Enter a string-:")
+m=reversed(n)
+if list(n)==list(m):
+    print(n,"is a palindrome string")
+else:
+    print(n,"is not a palindrome string")
+        
