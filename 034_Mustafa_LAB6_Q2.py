dbaf814a6c3452028b096e5de3c0ce52105d01c9
Kamaal <mkamaalg4@gmail.com>
2019-10-23 23:21:12 +0530

    check

---
+++ 034_Mustafa_LAB6_Q2.py
@@ -0,0 +1,12 @@
+#Name :       Mohd. Mustafa kamaal
+#Roll no.:    18BTCS034HY
+#Enrl no.:    A180571
+thisList = ['5','3','5','2','7','8','9','1','4','6']
+print(thisList," is the given list.")
+user_in = input("Enter 'm' to find minimum number or 'M' to find maximum number :")
+if user_in == 'm':
+    print(min(thisList)," is the minimum number.")
+elif user_in == 'M':
+    print(max(thisList)," is the maximum number.")
+else:
+    print("Enter 'm' or 'M' only")
---
+++ 034_Mustafa_LAB6_Q5.py
@@ -0,0 +1,16 @@
+# Name: Mohd Mustafa Kamaal
+# Roll no: 34
+# Enrl no: A180571
+# Batch: 1
+while 1:
+    year = input("Enter a year to check leap year :")
+    try:
+        year = int(year)
+        if year%4 == 0 or year%100 == 0 or year%400 == 0:
+            print(year," is a leap year")
+        else:
+            print(year," is not a leap year")
+        break
+    except ValueError:
+        print("Enter year in number only.")
+        continue
